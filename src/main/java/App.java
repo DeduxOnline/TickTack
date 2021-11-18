b944057b483f166163890156dfb877b08e3281cb
Dedux Online <94613375+DeduxOnline@users.noreply.github.com>
2021-11-18 14:06:43 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Ivan Muzyka");
         System.out.println("OK");
+        System.out.println("Today is a good day for Java programming.");//Den
     }
 }
